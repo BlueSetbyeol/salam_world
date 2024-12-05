504ffe18f133cfffed6a371fa8621926a9d03a88
BlueSetbyeol <setbyeol.c@gmail.com>
2024-12-05 10:14:42 +0100

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<header>
+				<h1 className="logo">Salam World</h1>
+			</header>
+
+			<nav className="navbar">
+				<ul>
+					<li>Apprendre à dire bonjour</li>
+				</ul>
+			</nav>
+
+			<main className="text-box">
+				<p>Que voulez vous dire ?</p>
+			</main>
+
+			<footer>Développé par la Salam World Team</footer>
+		</>
+	);
+}
+
+export default App;
