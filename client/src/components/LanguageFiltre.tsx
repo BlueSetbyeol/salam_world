8d7a3395b0c4662343a06947a832bc3e295e0e8b
elhedi <hheeddii87@gmail.com>
2024-12-05 15:28:15 +0100

    null

--- client/src/components/LanguageFiltre.tsx
+++ client/src/components/LanguageFiltre.tsx
@@ -1,28 +1,31 @@
 import { useEffect, useState } from "react";
 
 interface languageProps {
-    id:number;
-    language:string;
+	id: number;
+	language: string;
+	flag: string;
 }
 
-export default function LanguageFiltre () {
-    const [languages, setLanguages] = useState<languageProps[]>([]);
-    useEffect(() => {
-        fetch("http://localhost:3310/api/translate")
-        .then((res) => res.json())
-        .then((data) => setLanguages(data))
-        .catch((err) => console.error(err));
-    }, []);
+export default function LanguageFiltre() {
+	const [languages, setLanguages] = useState<languageProps[]>([]);
+	useEffect(() => {
+		fetch("http://localhost:3310/api/translate")
+			.then((res) => res.json())
+			.then((data) => setLanguages(data))
+			.catch((err) => console.error(err));
+	}, []);
 
-    return (
-
-        <label> 
-            Choisissez une langue :
-            <select >
-                {languages.map((langue) => (
-                    <option value={langue.language} key={langue.id}> {langue.language} </option>
-                )) }
-            </select>
-        </label>
-    )
+	return (
+		<label>
+			Choisissez une langue :
+			<select>
+				{languages.map((langue) => (
+					<option value={langue.language} key={langue.id}>
+						{" "}
+						{langue.language}
+					</option>
+				))}
+			</select>
+		</label>
+	);
 }
